2fb980e4e4e29393735dbef217a4703f2152788e
slightair <arksutite@gmail.com>
2014-03-08 18:31:15 +0900

    Create iOS app project

---
+++ iOS/HelloOno/HLOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HLOAppDelegate.h
+//  HelloOno
+//
+//  Created by slightair on 2014/03/08.
+//  Copyright (c) 2014年 slightair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/HelloOno/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloOno
+//
+//  Created by slightair on 2014/03/08.
+//  Copyright (c) 2014年 slightair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HLOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HLOAppDelegate class]));
+    }
+}
