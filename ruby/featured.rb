9c163a9033dc4c6cbd978c447fb4af217154cf0a
slightair <arksutite@gmail.com>
2014-03-08 18:23:56 +0900

    Add ruby sample script using nokogiri

---
+++ ruby/featured.rb
@@ -0,0 +1,14 @@
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('http://www.cookpad.com'))
+
+doc.xpath('//div[@id="past_featured_content"]//li/a').each do |item|
+  puts "#{item.text.chomp} - #{item.attributes['href'].text}"
+end
+
+puts
+
+doc.css('#past_featured_content li a').each do |item|
+  puts "#{item.text.chomp} - #{item.attributes['href'].text}"
+end
